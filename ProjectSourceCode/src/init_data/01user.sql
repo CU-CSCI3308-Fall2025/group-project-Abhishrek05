53411abaf0bd6104a92a42355058fbaf3b74da64
Aryan Pagar <arpa7666@colorado.edu>
2025-12-02 23:00:39 -0700

    handled streaks + deleting/adding calendar events to backend

--- ProjectSourceCode/src/init_data/01user.sql
+++ ProjectSourceCode/src/init_data/01user.sql
@@ -2,5 +2,8 @@ CREATE TABLE users (
     email VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(60) NOT NULL,
     username VARCHAR(50) PRIMARY KEY,
-    institution VARCHAR(50)
+    institution VARCHAR(50),
+    points INTEGER NOT NULL DEFAULT 0,
+    streak INTEGER NOT NULL DEFAULT 0,
+    lastlogin DATE
 );
